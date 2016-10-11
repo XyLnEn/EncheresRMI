58ca7410b7b312f493001db8b49159c52993926a
Liciax <aboubouille@gmail.com>
2016-10-11 09:56:53 +0200

    debutd'inscription

---
+++ src/ihm/ActionClient.java
@@ -0,0 +1,18 @@
+package ihm;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class ActionClient implements ActionListener {
+
+	public ActionClient() {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
