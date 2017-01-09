6bd2acc1621db170fe6a6c514950475a856d17a8
Théo Couraud <theo.couraud@laposte.net>
2017-01-09 15:56:06 +0100

    Clean commentaires

--- src/interfaces/IServeurVente.java
+++ src/interfaces/IServeurVente.java
@@ -8,26 +8,20 @@ import implementation.serveur.ObjetEnVente;
 
 public interface IServeurVente extends Remote{
 
-	/*** @author lenny
+	/**
 	 * inscrit un acheteur au serveur. L'acheteur est en attente tant qu'une enchere est en cours
-	 * 
-	 * @param pseudo
-	 * @param acheteur
 	 * @throws RemoteException
 	 */
 	public void inscriptionAcheteur(String pseudo, IAcheteur acheteur) throws RemoteException;
 	
-	/** @author lenny
+	/**
 	 * propose une enchere au serveur
-	 * @param prix
-	 * @param acheteur
 	 * @throws RemoteException
 	 */
 	public void rencherir(int prix, IAcheteur acheteur) throws RemoteException;
 	
-	/**@author lenny
+	/**
 	 * previens le serveur que le client ne souhaite pas rencherir ce tour
-	 * @param acheteur
 	 * @throws RemoteException
 	 */
 	public void tempsEcoule(IAcheteur acheteur) throws RemoteException;
